6b0b0114547566071870e6b1b22fed154057e13e
codeace110 <neri11032321@gmail.com>
2023-03-17 20:34:30 +0800

    adding codemirror

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>kodebreakers</title>
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kodebreakers</title>
+    <link href="https://fonts.googleapis.com/css?family=Figtree:400,600&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0-beta1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/codemirror.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/theme/dracula.min.css" rel="stylesheet">
+    <style>
+        .output-container {
+            height: 300px;
+            overflow: auto;
+        }
+    </style>
 </head>
-
-<body class="antialiased">
+<body>
     <div class="container my-5">
         <h1>Welcome to Kodebreakers!</h1>
         <p>This is a simple online code editor that you can use to learn how to write code in various programming languages.</p>
@@ -17,43 +23,45 @@
             <div class="col-md-6">
                 <h2>Code Editor</h2>
                 <p>Enter your code in the text area below, then click the "Run" button to see the output.</p>
-                <textarea id="editor" class="form-control"></textarea>
+                <textarea id="editor"></textarea>
                 <button onclick="runCode()" class="btn btn-primary my-3">Run</button>
             </div>
             <div class="col-md-6">
                 <h2>Output</h2>
                 <p>The output of your code will appear in the area below.</p>
-                <iframe id="output" class="border border-dark w-100" height="300"></iframe>
+                <div id="output-container" class="border border-dark output-container w-100"></div>
             </div>
         </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/codemirror.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/mode/javascript/javascript.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/edit/matchbrackets.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/edit/closebrackets.min.js"></script>
     <script>
         function runCode() {
-            var iframe = document.getElementById('output');
+            var outputContainer = document.getElementById('output-container');
             var code = document.getElementById('editor').value;
 
-            // Create a new window for the iframe
-            var iframeWindow = iframe.contentWindow;
-            var iframeDocument = iframeWindow.document;
-
-            // Clear the iframe content
-            iframeDocument.open();
-            iframeDocument.write('');
-            iframeDocument.close();
+            // Clear the output container
+            outputContainer.innerHTML = '';
 
             // Try to run the code and catch any errors
             try {
-                iframeWindow.eval(code);
+                var result = eval(code);
+                outputContainer.textContent = result;
             } catch (error) {
-                // If an error occurs, display it in the output iframe
-                iframeDocument.open();
-                iframeDocument.write('<p style="color:red;">Error: ' + error.message + '</p>');
-                iframeDocument.close();
+                // If an error occurs, display it in the output container
+                outputContainer.innerHTML = '<p style="color:red;">Error: ' + error.message + '</p>';
             }
-        };
+        }
+        
+        var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+            lineNumbers: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+            mode: "javascript",
+            theme: "dracula"
+        });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
-
 </html>
