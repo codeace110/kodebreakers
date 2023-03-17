26b9ea85104f48ba7d0cb72213fb255609ff5826
codeace110 <neri11032321@gmail.com>
2023-03-18 01:38:00 +0800

    redesign editor and output box

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,139 +9,210 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0-beta1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/codemirror.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/theme/dracula.min.css" rel="stylesheet">
+
     <style>
-        .output-container {
-            height: 300px;
-            overflow: auto;
-            color: red;
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
         }
+
+        h1,
+        h2 {
+            font-weight: bold;
+            text-align: center;
+        }
+
+        p {
+            text-align: center;
+        }
+
+        .top-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 20px;
+            margin-top: 20px;
+            background-color: #fff;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+            border-radius: 5px;
+            max-width: 800px;
+            margin: auto;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin: 20px 0;
+        }
+
+        .col {
+            flex: 1;
+            padding: 0 10px;
+        }
+
+        .col-md-6 {
+            flex-basis: 50%;
+            max-width: 50%;
+            padding: 0 10px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            background-color: #1e90ff;
+            color: #fff;
+            font-size: 16px;
+            cursor: pointer;
+            transition: background-color 0.3s ease-in-out;
+        }
+
+        .btn:hover {
+            background-color: #0077b3;
+        }
+
+        /****************************************/
+
 
         .editor-container {
+            width: 100%;
             height: 500px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            overflow: hidden;
+            display: flex;
         }
 
-        #editor,
-        .CodeMirror {
-            height: 100%;
-        }
-
-        /* Style the editor and output container */
-        .CodeMirror {
-            height: 100%;
-            font-family: "Figtree", sans-serif;
-            font-size: 1rem;
+        #editor {
+            flex: 1;
             border: none;
+            background-color: transparent;
+            font-family: monospace;
+            font-size: 14px;
+            resize: none;
+            padding: 50px;
         }
 
         .output-container {
-            height: 100%;
-            overflow: auto;
-            color: red;
-            border: none;
-        }
-
-        /* Style the container and body */
-        body {
-            background-color: #f8f8f8;
-        }
-
-        .container {
-            background-color: white;
+            width: 100%;
+            height: 500px;
+            margin-bottom: 20px;
             border-radius: 5px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-            padding: 2rem;
+            overflow: hidden;
+        }
+
+        #output-container {
+            padding: 50px;
+            width: 100%;
+            height: 100%;
+            border: none;
+            background-color: #fff;
+            font-family: monospace;
+            font-size: 14px;
+            overflow-y: none;
+            box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+
+        /* Media Queries */
+        @media screen and (max-width: 767px) {
+            .col-md-6 {
+                flex-basis: 100%;
+                max-width: 100%;
+            }
         }
     </style>
 </head>
 
 <body>
-    <div class="container my-5">
+    <div class="top-container">
         <h1>Welcome to Kodebreakers!</h1>
         <p>This is a simple online code editor that you can use to learn how to write code in various programming languages.</p>
 
-        <div class="container">
-        <button onclick="runCode()" class="btn btn-primary my-3">Run</button>
-            <div class="col">
-
-                <div class="col-md-6">
-                    <div class="col-md-6">
-                        <h2>Code Editor</h2>
-                        <p>Enter your code in the editor below, then click the "Run" button to see the output.</p>
-                        <div class="editor-container border border-dark">
-                            <textarea id="editor"></textarea>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="container">
-                     <h2>Output</h2>
-                        <p>The output of your code will appear in the area below.</p>
-                    <div class="col">
-                        <div class="col-md-3">
-                            <div id="output-container" class="border border-dark output-container w-100"></div>
-                        </div>
-                    </div>
-
-
-                </div>
-
-
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Code Editor</h2>
+                <p>Enter your code in the editor below, then click the "Run" button to see the output.</p>
+                <button onclick="runCode()" class="btn">Run</button>
             </div>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/codemirror.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/mode/javascript/javascript.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/edit/matchbrackets.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/edit/closebrackets.min.js"></script>
-            <script>
-                function runCode() {
-                    var outputContainer = document.getElementById('output-container');
-                    var code = editor.getValue();
+            <div class="col">
+                <h2>Output</h2>
+                <p>The output of your code will appear in the area below.</p>
+            </div>
+        </div>
+    </div>
 
-                    // Clear the output container
-                    outputContainer.innerHTML = '';
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="editor-container">
+                    <textarea id="editor"></textarea>
+                </div>
+            </div>
 
-                    // Redirect console.log() to the output container
-                    var oldLog = console.log;
-                    console.log = function(message) {
-                        var pre = document.createElement('pre');
-                        pre.innerText = message;
-                        outputContainer.appendChild(pre);
-                    };
+            <div class="col">
+                <div class="output-container">
+                    <div id="output-container"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/codemirror.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/mode/javascript/javascript.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/edit/matchbrackets.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/edit/closebrackets.min.js"></script>
+    <script>
+        function runCode() {
+            var outputContainer = document.getElementById('output-container');
+            var code = editor.getValue();
 
-                    // Try to run the code and catch any errors
-                    try {
-                        eval(code);
-                    } catch (error) {
-                        // If an error occurs, display it in the output container
-                        var p = document.createElement('p');
-                        p.innerText = 'Error: ' + error.message;
-                        p.style.color = 'red';
-                        outputContainer.appendChild(p);
-                    }
+            // Clear the output container
+            outputContainer.innerHTML = '';
 
-                    // Restore console.log()
-                    console.log = oldLog;
-                };
-                var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-                    lineNumbers: true,
-                    matchBrackets: true,
-                    autoCloseBrackets: true,
-                    mode: "javascript",
-                    theme: "dracula"
-                });
+            // Redirect console.log() to the output container
+            var oldLog = console.log;
+            console.log = function(message) {
+                var pre = document.createElement('pre');
+                pre.innerText = message;
+                outputContainer.appendChild(pre);
+            };
 
-                // Set the editor width and height
-                editor.setSize("100%", "100%");
+            // Try to run the code and catch any errors
+            try {
+                eval(code);
+            } catch (error) {
+                // If an error occurs, display it in the output container
+                var p = document.createElement('p');
+                p.innerText = 'Error: ' + error.message;
+                p.style.color = 'red';
+                outputContainer.appendChild(p);
+            }
 
-                // Set the output container height
-                var outputContainer = document.getElementById('output-container');
-                outputContainer.style.height = (window.innerHeight - 120) + 'px';
+            // Restore console.log()
+            console.log = oldLog;
+        };
+        var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+            lineNumbers: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+            mode: "javascript",
+            theme: "dracula"
+        });
 
-                // Listen for window resize events and adjust the output container height accordingly
-                window.addEventListener('resize', function() {
-                    outputContainer.style.height = (window.innerHeight - 120) + 'px';
-                });
-            </script>
+        // Set the editor width and height
+        editor.setSize("100%", "100%");
+
+        // Set the output container height
+        var outputContainer = document.getElementById('output-container');
+        outputContainer.style.height = (window.innerHeight - 120) + 'px';
+
+        // Listen for window resize events and adjust the output container height accordingly
+        window.addEventListener('resize', function() {
+            outputContainer.style.height = (window.innerHeight - 120) + 'px';
+        });
+    </script>
 </body>
 
 </html>
